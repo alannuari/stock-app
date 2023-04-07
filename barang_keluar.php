54bc15892efe3e2dd4781e88cfa79b40a08f2869
alannuari <alannuari372@gmail.com>
2023-04-07 15:16:16 +0700

    update admin page

--- barang_keluar.php
+++ barang_keluar.php
@@ -16,11 +16,13 @@ require 'get_barang_keluar.php';
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Aplikasi Management Stok Barang" />
+        <meta name="keywords" content="Management Stock, Stock App, Barang" />
+        <meta name="author" content="Alan Nuari" />
+        <link rel="shortcut icon" href="./images/icon.png" type="image/x-icon" />
         <title>Barang Keluar</title>
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
